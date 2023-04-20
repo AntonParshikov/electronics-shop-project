d3075987ae1c75972b63ca87d91380f648a4d7df
Anton Parshikov <tosha1703@gmail.com>
2023-04-20 21:06:11 +0300

    add hw1

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,18 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest as pytest
+
+from src.item import Item
+
+
+@pytest.fixture
+def data():
+    return Item('TV', 15000, 5)
+
+
+def test_calculate_total_price(data):
+    assert data.calculate_total_price() == 75000
+
+
+def test_apply_discount(data):
+    data.pay_rate = 0.5
+    assert data.apply_discount() == 7500
